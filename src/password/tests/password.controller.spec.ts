2a44b5f160b5a6268576b695d7c028b9ecb03cbe
Diogo Domene <ddomene@interaxa.com>
2020-04-27 22:16:17 -0300

    Adding jwtService to password test

--- src/password/tests/password.controller.spec.ts
+++ src/password/tests/password.controller.spec.ts
@@ -11,10 +11,12 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { defaultAdmin } from '../../scripts/seed/seeders/user/faker';
 import UserMocks from '../../users/tests/mocks'
 import { JwtModuleRegister } from '../password.module';
+import { JwtService } from '@nestjs/jwt';
 
 describe('Auth Controller', () => {
   let controller: PasswordController;
   let userRepository: Repository<UserEntity>
+  let jwtService: JwtService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -33,6 +35,7 @@ describe('Auth Controller', () => {
     }).compile()
 
     controller = module.get<PasswordController>(PasswordController);
+    jwtService = module.get<JwtService>(JwtService);
     userRepository = module.get<Repository<UserEntity>>(getRepositoryToken(UserEntity))
   });
 
@@ -53,7 +56,10 @@ describe('Auth Controller', () => {
     const { create: { valid } } = UserMocks
     jest.spyOn(userRepository, 'update').mockResolvedValueOnce(true as never)
     jest.spyOn(userRepository, 'findOne').mockResolvedValueOnce(valid as never)
-    const response = await controller.reset( { email, password } )
+    const response = await controller.reset(
+      { password },
+      { authorization: jwtService.sign({ email }) }
+    )
     expect(typeof response.message).toBe('string')
   });
 });
